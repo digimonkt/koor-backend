9a3d0fa2c09c6dc8671a0a5dbb9e5d7d95577088
praveenvaidhya <praveen.vaidhya@digimonk.in>
2023-01-17 12:45:14 +0530

    #2 - Create Media Model

---
+++ project_meta/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from django.utils.translation import gettext as _
+
+from core.models import BaseModel, upload_directory_path
+
+
+class Media(BaseModel, models.Model):
+    MEDIA_TYPE_CHOICE = (
+        ('image', "Image"),
+        ('video', "Video"),
+        ('document', "Document"),
+    )
+
+    file_path = models.FileField(
+        verbose_name=_('File Path'),
+        unique=True,
+        upload_to=upload_directory_path,
+        db_column="file_path",
+    )
+    media_type = models.CharField(
+        verbose_name=_('Media Type'),
+        max_length=250,
+        db_column="media_type",
+        choices=MEDIA_TYPE_CHOICE,
+        default='image'
+    )
+
+    def __str__(self):
+        return self.file_path
+
+    class Meta:
+        verbose_name = "Media"
+        verbose_name_plural = "Media"
+        db_table = "Media"
