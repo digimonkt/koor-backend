c3b2d6fc7fe8359a5f5a31aa8dba290c3d1e4dc2
praveenvaidhya <praveen.vaidhya@digimonk.in>
2023-01-18 11:36:19 +0530

    #2 - Create Resume Model

--- job_seeker/models/details.py
+++ job_seeker/models/details.py
@@ -4,6 +4,7 @@ from django.utils.translation import gettext as _
 from core.models import (
     BaseModel, SoftDeleteModel
 )
+from project_meta.models import Media
 from users.models import User
 
 
@@ -13,7 +14,7 @@ class EducationRecord(BaseModel, SoftDeleteModel, models.Model):
         User,
         verbose_name=_('User'),
         on_delete=models.CASCADE,
-        related_name='%(app_label)s_%(class)sJobSeeker'
+        related_name='%(app_label)s_%(class)sEducationRecord'
     )
     title = models.CharField(
         verbose_name=_('Title'),
@@ -54,3 +55,83 @@ class EducationRecord(BaseModel, SoftDeleteModel, models.Model):
         verbose_name = "Education Record"
         verbose_name_plural = "Education Records"
         db_table = "EducationRecord"
+
+
+class EmploymentRecord(BaseModel, SoftDeleteModel, models.Model):
+
+    user = models.ForeignKey(
+        User,
+        verbose_name=_('User'),
+        on_delete=models.CASCADE,
+        related_name='%(app_label)s_%(class)sEmploymentRecord'
+    )
+    title = models.CharField(
+        verbose_name=_('Title'),
+        max_length=255,
+        db_column="title",
+    )
+    start_date = models.DateField(
+        verbose_name=_('Start Date'),
+        db_column='start_date'
+    )
+    end_date = models.DateField(
+        verbose_name=_('End Date'),
+        blank=True,
+        null=True,
+        db_column='end_date'
+    )
+    present = models.BooleanField(
+        verbose_name=_('Present'),
+        null=True,
+        blank=True,
+        default=True,
+        db_column="present",
+    )
+    organization = models.CharField(
+        verbose_name=_('Organization'),
+        max_length=255,
+        db_column="organization",
+    )
+    description = models.TextField(
+        verbose_name=_('Description'),
+        null=True,
+        blank=True,
+        db_column="description",
+    )
+
+    def __str__(self):
+        return str(self.title) + "(" + str(self.user) + ")"
+
+    class Meta:
+        verbose_name = "Employment Record"
+        verbose_name_plural = "Employment Records"
+        db_table = "EmploymentRecord"
+
+
+class Resume(BaseModel, SoftDeleteModel, models.Model):
+
+    user = models.ForeignKey(
+        User,
+        verbose_name=_('User'),
+        on_delete=models.CASCADE,
+        related_name='%(app_label)s_%(class)sResume'
+    )
+    title = models.CharField(
+        verbose_name=_('Title'),
+        max_length=255,
+        db_column="title",
+    )
+    file_path = models.ForeignKey(
+        Media,
+        verbose_name=_('File Path'),
+        on_delete=models.CASCADE,
+        related_name='%(app_label)s_%(class)sFilePath'
+    )
+
+    def __str__(self):
+        return str(self.title) + "(" + str(self.user) + ")"
+
+    class Meta:
+        verbose_name = "Resume"
+        verbose_name_plural = "Resumes"
+        db_table = "Resume"
