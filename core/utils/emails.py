529c99e94644ae0831bcc8d130084f1f85c720c4
praveenvaidhya <praveen.vaidhya@digimonk.in>
2023-01-19 16:36:42 +0530

    #3 - Modified generic function with comments.

--- core/utils/emails.py
+++ core/utils/emails.py
@@ -13,17 +13,17 @@ def get_email_object(subject, email_template_name, context, to_email, content_su
     """
     logger = logging.getLogger(__name__)
     try:
-        smtp_setting_instance = SMTPSetting.objects.last()
-        host = smtp_setting_instance.smtp_host
-        host_user = smtp_setting_instance.smtp_user
-        host_password = smtp_setting_instance.smtp_password
-        host_port = smtp_setting_instance.smtp_port
+        smtp_setting = SMTPSetting.objects.last()  # Get last SMTP detail form database.
+        host = smtp_setting.smtp_host
+        host_user = smtp_setting.smtp_user
+        host_password = smtp_setting.smtp_password
+        host_port = smtp_setting.smtp_port
         mail_obj = EmailBackend(host=host, port=host_port, password=host_password, username=host_user, use_tls=True,
-                                timeout=10)
+                                timeout=10)  # Create mail objects with the SMTP details.
 
-        email_template = get_template(email_template_name).render(context)
+        email_template = get_template(email_template_name).render(context)  # Create email templates with valid context.
 
-        if 'type' in kwargs.keys():
+        if 'type' in kwargs.keys():  # Check any attachment are required or not.
             email_msg = mail.EmailMessage(
                 subject=subject,
                 body=email_template,
@@ -39,9 +39,9 @@ def get_email_object(subject, email_template_name, context, to_email, content_su
                 to=[to_email],
             )
         email_msg.content_subtype = content_subtype
-        mail_obj.send_messages([email_msg])
-        mail_obj.close()
-        return True
+        mail_obj.send_messages([email_msg])  # send email.
+        mail_obj.close()  # Close mail object after send email.
+        return True  # Rerun True if email send successfully.
     except Exception as e:
-        logger.exception("Exception occurred", exc_info=True)
-        return None
+        logger.exception("Exception occurred", exc_info=True)  # Create error log.
+        return None  # Return None if mail not send.
