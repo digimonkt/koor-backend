f70179c8631f7ac1456059689154c158139ae68a
kevalrajpalknight <kevalprogram100@gmail.com>
2023-01-20 18:33:02 +0530

    #1 - Updated swagger setting with docs branch

--- KOOR/urls.py
+++ KOOR/urls.py
@@ -14,7 +14,7 @@ schema_view = get_schema_view(
    openapi.Info(
       title="Koor API",
       default_version='v1',
-      description=render_to_string(os.path.join(DJANGO_CONFIGURATION.BASE_DIR,"..",'README.md')),
+      description=render_to_string('docs\index.md'),
       terms_of_service="https://www.google.com/policies/terms/",
       contact=openapi.Contact(email="info@digimonk.in"),
    ),
