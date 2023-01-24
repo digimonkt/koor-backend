0e064bb7612cc56c08f036757ce8b3a0f3c22ac9
praveenvaidhya <praveen.vaidhya@digimonk.in>
2023-01-24 11:31:35 +0530

    #5 - change path name

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,6 @@
 # IMPORT SOME USEFUL PACKAGES.
 from django.urls import path
+
 # IMPORT SOME USEFUL VIEWS FILE.
 from . import views
 
@@ -8,6 +9,6 @@ urlpatterns = [
 
     path('', views.CreateUserView.as_view(), name="create_user_view_link"),
 
-    path('session', views.CreateSessionView.as_view(), name="create_session_view_link"),
+    path('/session', views.CreateSessionView.as_view(), name="create_session_view_link"),
 
 ]
