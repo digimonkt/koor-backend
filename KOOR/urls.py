ff62142bfab734bc1f14733c6c30603908d216ca
praveenvaidhya <praveen.vaidhya@digimonk.in>
2023-01-25 17:37:14 +0530

    #4 - modify urls path

--- KOOR/urls.py
+++ KOOR/urls.py
@@ -28,5 +28,5 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
-    path("api/v1/user/", include(('users.urls', 'users'), namespace='user_app_link')),
+    path("api/v1/user", include(('users.urls', 'users'), namespace='user_app_link')),
 ]
