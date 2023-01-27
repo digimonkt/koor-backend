68bcc0501e43735df1b65bc39d79ce3e89bbd10b
praveenvaidhya <praveen.vaidhya@digimonk.in>
2023-01-27 10:10:37 +0530

    #4 - remove custom exception handler

--- KOOR/settings/local.py
+++ KOOR/settings/local.py
@@ -212,7 +212,6 @@ class Common(Configuration):
     # https://www.django-rest-framework.org/
 
     REST_FRAMEWORK = {
-        'EXCEPTION_HANDLER': 'core.utils.CustomExceptionHandler',
         'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
         'PAGE_SIZE': 20,
         'DATETIME_FORMAT': '%Y-%m-%dT%H:%M:%S%z',
--- core/utils/__init__.py
+++ core/utils/__init__.py
@@ -1,2 +1,1 @@
 from .emails import SMTPSetting
-from .custom_exception_handler import CustomExceptionHandler
