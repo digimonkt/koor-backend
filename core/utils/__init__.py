7cfdb8e568f9ff993d544aacdf043954483c952c
praveenvaidhya <praveen.vaidhya@digimonk.in>
2023-02-01 12:42:03 +0530

    #39 - modify call name

--- core/utils/__init__.py
+++ core/utils/__init__.py
@@ -1,3 +1,3 @@
 from .emails import SMTPSetting
 from .custom_validation_error import CustomValidationError
-from .get_user_id import get_user_instance
+from .get_user_instance import get_user_instance
