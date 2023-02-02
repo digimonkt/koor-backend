ccc8d7d40d1a88597708b573506691e16c388e79
kevalrajpalknight <kevalprogram100@gmail.com>
2023-02-02 17:17:41 +0530

    #56 - Added BaseModel

--- core/models.py
+++ core/models.py
@@ -1,4 +1,15 @@
 from datetime import date
+from django.utils.translation import gettext as _
+
+from model_utils import (
+    FieldTracker,
+)
+from model_utils.models import (
+    UUIDModel
+)
+
+
+# Model Mixin
 
 def upload_directory_path(instance, filename):
     """
@@ -6,3 +17,32 @@ def upload_directory_path(instance, filename):
     Example:- YYYT-MM-DD/instance_name/file_name
     """
     return '{0}/{1}/{2}'.format(date.today(), instance, filename)
+
+class BaseModel(UUIDModel):
+    """
+    An abstract base class model that provides self updating, fields.
+
+    This model has:
+        - models_utils.UUIDModel that ``id`` field on any model that inherits from it which will be the primary key.
+        - models_utils.FieldTracker that can be added to a model to track changes in model fields.
+
+    Reference: https://django-model-utils.readthedocs.io/en/latest/
+
+    Example:
+    class Post(models.Model):
+        title = models.CharField(max_length=100)
+        tracker = FieldTracker()
+    >>> a = Post.objects.create(title='First Post')
+    >>> a.title = 'Welcome'
+    >>> a.tracker.previous('title')
+    u'First Post'
+    >>> a.tracker.has_changed('title')
+    True
+    >>> a.tracker.has_changed('body')
+    False
+    """
+    # A FieldTracker allows querying for field changes since a model instance was last saved.
+    tracker = FieldTracker()
+
+    class Meta:
+        abstract = True
