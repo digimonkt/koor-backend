8fdce71657b89133e1266d180d8caaad3b9daf0d
kevalrajpalknight <kevalprogram100@gmail.com>
2023-02-02 22:47:28 +0530

    #2 - Inherit models from SlugBaseModel

--- project_meta/models.py
+++ project_meta/models.py
@@ -1,9 +1,8 @@
 from django.db import models
 from django.utils.translation import gettext as _
-from django.template.defaultfilters import slugify
 
 from core.models import (
-    BaseModel, SoftDeleteModel, upload_directory_path,
+    BaseModel, SlugBaseModel, upload_directory_path,
 )
 
 # Create your models here.
@@ -45,7 +44,7 @@ class Media(BaseModel, models.Model):
         verbose_name_plural = "Media"
         db_table = "Media"
 
-class Tag(BaseModel, SoftDeleteModel, models.Model):
+class Tag(SlugBaseModel, models.Model):
     """
     This table is used to store details about a tag.
 
@@ -53,60 +52,8 @@ class Tag(BaseModel, SoftDeleteModel, models.Model):
     - `title`: A string representing the name of the tag. 
     - `slug`: A string representing the slug for the tag, used in URLs or filtering process.
     """
-    title = models.CharField(
-        verbose_name=_('Title'),
-        max_length=255,
-        db_column="title",
-    )
-    slug = models.SlugField(
-        unique=True,
-        null=True,
-        blank=True,
-        db_column="slug",
-    )
-
-    def __str__(self):
-        return self.title
-
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.title)
-        return super().save(*args, **kwargs)
 
     class Meta:
         verbose_name = "Tag"
         verbose_name_plural = "Tags"
         db_table = "Tag"
-
-class Language(BaseModel, SoftDeleteModel, models.Model):
-    """ 
-    This table stores information about languages in a Django Model.
-
-    Columns: 
-    - `title`: A string representing the name of the language. 
-    - `slug`: A string representing the slug for the language, used in URLs and filtering record.
-    """
-    title = models.CharField(
-        verbose_name=_('Title'),
-        max_length=255,
-        db_column="title",
-    )
-    slug = models.SlugField(
-        unique=True,
-        null=True,
-        blank=True,
-        db_column="slug",
-    )
-
-    def __str__(self):
-        return self.title
-
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.title)
-        return super().save(*args, **kwargs)
-
-    class Meta:
-        verbose_name = "Language"
-        verbose_name_plural = "Languages"
-        db_table = "Language"
