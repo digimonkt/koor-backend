5cbdab043e6613ac597531d708b3e1b42be60f13
kevalrajpalknight <kevalprogram100@gmail.com>
2023-02-03 01:31:51 +0530

    #2 - Create JobCategory model

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils.translation import gettext as _
 
-# Create your models here.
+from core.models import (
+    SlugBaseModel
+)
+from users.models import TimeStampedModel
+
+
+class JobCategory(SlugBaseModel, TimeStampedModel, models.Model):
+    """
+    This table is used to store details about a Job Category.
+
+    Columns: 
+    - `title`: A string representing the name of the tag. 
+    - `slug`: A string representing the slug for the tag, used in URLs or filtering process.
+    """
+    class Meta:
+        verbose_name = "Job Category"
+        verbose_name_plural = "Job Categories"
+        db_table = "JobCategory"
