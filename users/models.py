980fcba7d115e1cc38bbd8b052743dc96837e652
kevalrajpalknight <kevalprogram100@gmail.com>
2023-02-02 23:19:49 +0530

    #2 - Create TimeStampedModel

--- users/models.py
+++ users/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext as _
 
+from model_utils import models as misc_models
+
 from core.models import (
     BaseModel, SoftDeleteModel
 )
@@ -82,3 +84,33 @@ class User(AbstractUser, BaseModel, SoftDeleteModel):
         verbose_name_plural = "Users"  # display table name as plural
         db_table = 'User'  # table name in DB
 
+class TimeStampedModel(misc_models.TimeStampedModel, models.Model):
+    """
+    This abstract base model is used to store timestamps for model creation and modification in a Django Model.
+
+    Columns:
+    - `created`: A datetime object representing the creation time of the model.
+    - `modified`: A datetime object representing the last modification time of the model.
+    - `created_by`: A string representing the user who created the model.
+    - `modified_by`: A string representing the user who last modified the model.
+    """
+
+    created_by = models.ForeignKey(
+        User,
+        verbose_name=_('Created By'),
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='%(app_label)s_%(class)s_created_by'
+    )
+    modified_by = models.ForeignKey(
+        User,
+        verbose_name=_('Modified By'),
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="%(app_label)s_%(class)s_modified_by",
+    )
+
+    class Meta:
+        abstract = True
