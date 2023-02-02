a1753d5e082f3f5f59d5d8c7f969b44a18d96361
kevalrajpalknight <kevalprogram100@gmail.com>
2023-02-02 23:58:36 +0530

    #2 - Alter field relationship

--- users/models.py
+++ users/models.py
@@ -62,7 +62,7 @@ class User(AbstractUser, BaseModel, SoftDeleteModel):
         db_column="role",
         choices=ROLE_TYPE_CHOICE
     )
-    image = models.ForeignKey(
+    image = models.OneToOneField(
         Media,
         verbose_name=_('Image'),
         null=True,
