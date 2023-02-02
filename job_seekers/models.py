2df5486e103467b68ce57fbd3d850e90d44bdbb5
kevalrajpalknight <kevalprogram100@gmail.com>
2023-02-03 01:02:59 +0530

    #2 - Create Resume model

--- job_seekers/models.py
+++ job_seekers/models.py
@@ -5,7 +5,7 @@ from core.models import (
     BaseModel, SoftDeleteModel
 )
 from users.models import User, TimeStampedModel
-
+from project_meta.models import Media
 
 class EducationRecord(BaseModel, SoftDeleteModel, TimeStampedModel, models.Model):
     """
@@ -119,3 +119,39 @@ class EmploymentRecord(BaseModel, SoftDeleteModel, TimeStampedModel, models.Mode
         verbose_name = "Employment Record"
         verbose_name_plural = "Employment Records"
         db_table = "EmploymentRecord"
+
+class Resume(BaseModel, SoftDeleteModel, TimeStampedModel, models.Model):
+    """
+    This Django model class represents the resume for a job seeker. The fields are as follows:
+
+    - `user`: The user who this resume belongs to.
+    - `title`: The title of the resume.
+    - `file_path`: The file containing the resume.
+    """
+    user = models.ForeignKey(
+        User,
+        verbose_name=_('User'),
+        on_delete=models.CASCADE,
+        db_column="user",
+        related_name='%(app_label)s_%(class)s_user'
+    )
+    title = models.CharField(
+        verbose_name=_('Title'),
+        max_length=255,
+        db_column="title",
+    )
+    file_path = models.OneToOneField(
+        Media,
+        verbose_name=_('File Path'),
+        on_delete=models.CASCADE,
+        db_column="file_path",
+        related_name='%(app_label)s_%(class)s_file_path'
+    )
+
+    def __str__(self):
+        return str(self.title) + "(" + str(self.user) + ")"
+
+    class Meta:
+        verbose_name = "Resume"
+        verbose_name_plural = "Resumes"
+        db_table = "Resume"
