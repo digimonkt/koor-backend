515241679d0377936adee42774860bd00694cc39
praveenvaidhya <praveen.vaidhya@digimonk.in>
2023-02-06 17:33:25 +0530

    #5 - added create session url path

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import CreateUserView
+from .views import CreateUserView, CreateSessionView
 
 app_name = "users"
 
 urlpatterns = [
-    path('', CreateUserView.as_view(), name="create_user")
+    path('', CreateUserView.as_view(), name="create_user"),
+
+    path('/session', CreateSessionView.as_view(), name="create_session")
 ]
