763293995cdf5f300dbc93ccb66306e587bc94ab
praveenvaidhya <praveen.vaidhya@digimonk.in>
2023-02-07 18:49:06 +0530

    #7 - create get user detail view

--- users/views.py
+++ users/views.py
@@ -2,38 +2,62 @@ from rest_framework import (
     status, generics, serializers,
     response, permissions
 )
-from core.tokens import SessionTokenObtainPairSerializer
 
+from core.tokens import SessionTokenObtainPairSerializer
 from .models import UserSession, User
 from .serializers import (
     CreateUserSerializers,
-    CreateSessionSerializers
-    )
+    CreateSessionSerializers,
+    JobSeekerDetailSerializers,
+    EmployerDetailSerializers
+)
+
 
 def create_user_session(request, user):
-        """
-        `create_user_session` creates a new UserSession object for the given request. It retrieves the IP address from the request and stores the user agent in the agent field.
-        """
-        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-        if x_forwarded_for:
-            IPAddr = x_forwarded_for.split(',')[0]
-        else:
-            IPAddr = request.META.get('REMOTE_ADDR')
-        agent = {'User-Agent': request.headers.get('User-Agent')}
-        user_session = UserSession.objects.create(
-            user=user, 
-            ip_address=IPAddr, 
-            agent=agent
-        )
-        user_session.save()
-        return user_session
+    """
+    `create_user_session` creates a new UserSession object for the given request. It retrieves the IP address from
+    the request and stores the user agent in the agent field.
+    """
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        IPAddr = x_forwarded_for.split(',')[0]
+    else:
+        IPAddr = request.META.get('REMOTE_ADDR')
+    agent = {'User-Agent': request.headers.get('User-Agent')}
+    user_session = UserSession.objects.create(
+        user=user,
+        ip_address=IPAddr,
+        agent=agent
+    )
+    user_session.save()
+    return user_session
+
 
 class UserView(generics.GenericAPIView):
+    """
+    UserView is a generic class-based view that provides implementation for handling user data through APIs.
+
+    Attributes:
+        serializer_class (CreateUserSerializers): Serializer class to be used for serializing user data.
+        permission_classes (List[permissions.AllowAny]): List of permission classes to be used for validating user
+        access.
+    """
     serializer_class = CreateUserSerializers
     permission_classes = [permissions.AllowAny]
 
     def post(self, request):
-        context = dict()  
+        """
+        Creates a new user, saves the password and creates a session for the user.
+        
+        Parameters:
+            request (django.http.request.HttpRequest): The request object containing data for user creation.
+        
+        Returns:
+            django.http.response.Response: HTTP response with status 201 and "User Created Successfully" message and 
+            access and refresh tokens in header if user is created successfully. If user is not created due to invalid
+            data, returns HTTP response with status 400 and error message.
+        """
+        context = dict()
         serializer = self.serializer_class(data=request.data)
         try:
             serializer.is_valid(raise_exception=True)
@@ -41,7 +65,7 @@ class UserView(generics.GenericAPIView):
             user = User.objects.get(id=serializer.data['id'])
             user.set_password(serializer.data['password'])
             user.save()
-            user_session = create_user_session(request, user,)
+            user_session = create_user_session(request, user, )
 
             token = SessionTokenObtainPairSerializer.get_token(
                 user=user,
@@ -57,6 +81,56 @@ class UserView(generics.GenericAPIView):
             return response.Response(
                 data=serializer.errors,
                 status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def get(self, request):
+        """
+        Retrieve user data based on user ID. If the user is authenticated, the user data is retrieved either by using
+        the user ID provided in the GET request or the ID of the authenticated user. If the user is not authenticated,
+        an error message is returned.
+
+        Args:
+            request (Request): The incoming request data.
+
+        Returns:
+            Response: The response containing the user data if authenticated and user data is found, otherwise an error
+            message.
+
+        Raises:
+            Exception: If an error occurs while retrieving the user data.
+        """
+        context = dict()
+        if request.user and request.user.is_authenticated:
+            context = dict()
+            user_id = request.GET.get('userId', None)
+            try:
+                if not user_id:
+                    user_id = request.user.id
+                user_data = User.objects.get(id=user_id)
+                if user_data.role == "job_seeker":
+                   
+                    get_data = JobSeekerDetailSerializers(user_data)
+                    context["data"] = get_data.data
+
+                elif user_data.role == "employer":
+                    get_data = EmployerDetailSerializers(user_data)
+                    context["data"] = get_data.data
+                
+                return response.Response(
+                    data=context,
+                    status=status.HTTP_200_OK
+                )
+            except Exception as e:
+                context["error"] = str(e)
+                return response.Response(
+                    data=context,
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+        else:
+            context["detail"] = "Authentication credentials were not provided."
+            return response.Response(
+                data=context,
+                status=status.HTTP_401_UNAUTHORIZED
             )
 
 
@@ -79,7 +153,7 @@ class CreateSessionView(generics.GenericAPIView):
     permission_classes = [permissions.AllowAny]
 
     def post(self, request):
-        context = dict()  
+        context = dict()
         serializer = self.serializer_class(data=request.data)
         try:
             serializer.is_valid(raise_exception=True)
