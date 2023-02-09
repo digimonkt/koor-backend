de96b378d8f13098ba30cfa3a7ab4bf48b790442
praveenvaidhya <praveen.vaidhya@digimonk.in>
2023-02-09 16:53:39 +0530

    #40 - create jobs view

--- employers/views.py
+++ employers/views.py
@@ -7,7 +7,8 @@ from rest_framework import (
 from user_profile.models import EmployerProfile
 
 from .serializers import (
-    UpdateAboutSerializers
+    UpdateAboutSerializers,
+    CreateJobsSerializers
 )
 
 
@@ -27,7 +28,7 @@ class UpdateAboutView(generics.GenericAPIView):
         - permission_classes (list): The permission classes required for accessing this view, where only authenticated 
                                       users are allowed.
     """
-    
+
     serializer_class = UpdateAboutSerializers
     permission_classes = [permissions.IsAuthenticated]
 
@@ -46,5 +47,59 @@ class UpdateAboutView(generics.GenericAPIView):
         except serializers.ValidationError:
             return response.Response(
                 data=serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+
+class CreateJobsView(generics.CreateAPIView):
+    """
+    View class for creating job details.
+
+    This view class is based on the `generics.CreateAPIView` class and extends its functionality to handle the creation
+    of job details objects. The view requires authentication and only allows users with the "employer" role to create
+    job details. The view uses the `CreateJobsSerializers` serializer class to validate and save the job details.
+
+    In case of a successful creation, the view returns a response with a status code of `HTTP_201_CREATED` and a
+    message indicating that the job was added successfully.
+
+    In case of validation errors, the view returns a response with a status code of `HTTP_400_BAD_REQUEST` and a message
+    indicating the validation errors.
+
+    In case of unauthorized access, the view returns a response with a status code of `HTTP_401_UNAUTHORIZED` and a
+    message indicating that the user is not authorized to create a job.
+
+    In case of any other exceptions, the view returns a response with a status code of `HTTP_400_BAD_REQUEST` and a
+    message indicating the exception that occurred.
+    """
+
+    serializer_class = CreateJobsSerializers
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request):
+        context = dict()
+        serializer = self.serializer_class(data=request.data)
+        try:
+            if self.request.user.role == "employer":
+                serializer.is_valid(raise_exception=True)
+                serializer.save(user=self.request.user)
+                context["message"] = "Job added successfully."
+                return response.Response(
+                    data=context,
+                    status=status.HTTP_201_CREATED
+                )
+            else:
+                context['message'] = "You are not authorized for create job."
+                return response.Response(
+                    data=context,
+                    status=status.HTTP_401_UNAUTHORIZED
+                )
+        except serializers.ValidationError:
+            return response.Response(
+                data=serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        except Exception as e:
+            return response.Response(
+                data=str(e),
                 status=status.HTTP_400_BAD_REQUEST
             )
