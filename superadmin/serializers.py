7c93586468133c4dbaf8205d737c507570e70e59
praveenvaidhya <praveen.vaidhya@digimonk.in>
2023-02-09 19:17:25 +0530

    #65 - create country serializers

---
+++ superadmin/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+
+from project_meta.models import (
+    Country
+)
+
+
+class CreateCountrySerializers(serializers.ModelSerializer):
+    """
+    Serializer class for the `Country` model.
+
+    The `CreateCountrySerializers` class extends `serializers.ModelSerializer`
+    and is used to create instances of the `Country` model. It defines the fields
+    that should be included in the serialized representation of the model, including
+    'title', 'currency_code', 'country_code', 'iso_code2', and 'iso_code3'.
+    """
+    class Meta:
+        model = Country
+        fields = ['title', 'currency_code', 'country_code', 'iso_code2', 'iso_code3']
