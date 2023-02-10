df11524daf5e515dfe28e267ad45e68c1d35320d
praveenvaidhya <praveen.vaidhya@digimonk.in>
2023-02-10 18:53:54 +0530

    #18 - create education level view

--- superadmin/views.py
+++ superadmin/views.py
@@ -9,11 +9,12 @@ from jobs.models import (
     JobCategory
     )
 from project_meta.models import (
-    Country, City
+    Country, City, EducationLevel
     )
 
 from .serializers import (
-    CountrySerializers, CitySerializers, JobCategorySerializers
+    CountrySerializers, CitySerializers, JobCategorySerializers,
+    EducationLevelSerializers
     )
 
 
@@ -201,7 +202,7 @@ class JobCategoryView(generics.ListAPIView):
         Handle POST request to create a new job category.
         The request must contain valid data for the job category to be created.
 
-        Only users with `admin` role and `is_staff` attribute set to True are authorized to create a job category.
+        Only users with `is_staff` attribute set to True are authorized to create a job category.
 
         Returns:
             - HTTP 201 CREATED with a message "JobCategory added successfully" if the job category is created
@@ -220,6 +221,81 @@ class JobCategoryView(generics.ListAPIView):
                 serializer.is_valid(raise_exception=True)
                 serializer.save()
                 context["message"] = "Job category added successfully."
+                return response.Response(
+                    data=context,
+                    status=status.HTTP_201_CREATED
+                )
+            else:
+                context['message'] = "You do not have permission to perform this action."
+                return response.Response(
+                    data=context,
+                    status=status.HTTP_401_UNAUTHORIZED
+                )
+        except serializers.ValidationError:
+            return response.Response(
+                data=serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        except Exception as e:
+            context['message'] = str(e)
+            return response.Response(
+                data=context,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+            
+class EducationLevelView(generics.ListAPIView):
+    """
+    A view for displaying a list of education levels .
+
+    Attributes:
+        - permission_classes ([permissions.IsAuthenticated]): List of permission classes that the view requires. In this
+            case, only authenticated users are allowed to access the view.
+
+        - serializer_class (EducationLevelSerializers): The serializer class used for data validation and serialization.
+
+        - queryset (QuerySet): The queryset that the view should use to retrieve the countries. By default, it is set
+            to retrieve all countries using `EducationLevel.objects.all()`.
+
+        - filter_backends ([filters.SearchFilter]): List of filter backends to use for filtering the queryset. In this
+            case, only `SearchFilter` is used.
+
+        - search_fields (list): List of fields to search for in the queryset. In this case, the field is "title".
+
+        - pagination_class (CustomPagination): The pagination class to use for paginating the queryset results.
+
+    """
+
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = EducationLevelSerializers
+    queryset = EducationLevel.objects.all()
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title']
+    pagination_class = CustomPagination
+
+    def post(self, request):
+        """
+        Handle POST request to create a new education level.
+        The request must contain valid data for the education level to be created.
+
+        Only users with `is_staff` attribute set to True are authorized to create a education level.
+
+        Returns:
+            - HTTP 201 CREATED with a message "EducationLevel added successfully" if the education level is created
+            successfully.
+            - HTTP 400 BAD REQUEST with error message if data validation fails.
+            - HTTP 401 UNAUTHORIZED with a message "You do not have permission to perform this action." if the user is
+            not authorized.
+
+        Raises:
+            Exception: If an unexpected error occurs during the request handling.
+        """
+        context = dict()
+        serializer = self.serializer_class(data=request.data)
+        try:
+            if self.request.user.is_staff:
+                serializer.is_valid(raise_exception=True)
+                serializer.save()
+                context["data"] = serializer.data
                 return response.Response(
                     data=context,
                     status=status.HTTP_201_CREATED
