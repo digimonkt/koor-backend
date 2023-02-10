701c80cff0cffb6a57ff7fbd889da988a5393814
praveenvaidhya <praveen.vaidhya@digimonk.in>
2023-02-10 17:15:03 +0530

    #69 import slugify package

--- project_meta/models.py
+++ project_meta/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils.translation import gettext as _
+from django.template.defaultfilters import slugify
 
 from core.models import (
     BaseModel, SlugBaseModel, upload_directory_path
