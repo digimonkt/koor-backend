58979f9a1a75f4aaf3c2aacd8fbe991fe94aea8c
praveenvaidhya <praveen.vaidhya@digimonk.in>
2023-02-15 17:44:57 +0530

    #93 - create get job detail url path

--- job_seekers/urls.py
+++ job_seekers/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import UpdateAboutView, JobSearchView
+from .views import UpdateAboutView, JobSearchView, GetJobDetailsView
 
 app_name = "job_seekers"
 
@@ -9,5 +9,7 @@ urlpatterns = [
     path('/about-me', UpdateAboutView.as_view(), name="update_about"),
     
     path('/job-search', JobSearchView.as_view(), name="job_search"),
+    
+    path('/get-job-details', GetJobDetailsView.as_view(), name="get_job_details"),
 
 ]
