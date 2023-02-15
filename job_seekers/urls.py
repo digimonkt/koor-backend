2ff18e872e6a357bf679894aa76ec41712f9c075
praveenvaidhya <praveen.vaidhya@digimonk.in>
2023-02-15 13:10:04 +0530

    #92 - create job search url path

--- job_seekers/urls.py
+++ job_seekers/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
-from .views import UpdateAboutView
+from .views import UpdateAboutView, JobSearchView
 
 app_name = "job_seekers"
 
 urlpatterns = [
 
     path('/about-me', UpdateAboutView.as_view(), name="update_about"),
+    
+    path('/job-search', JobSearchView.as_view(), name="job_search"),
 
 ]
