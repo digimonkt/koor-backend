2e12919c10c5d0e859ba09e6bdf307e692d42371
praveenvaidhya <praveen.vaidhya@digimonk.in>
2023-02-16 17:36:04 +0530

    #106 - update image url path

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 
-from .views import UserView, CreateSessionView, DeleteSessionView
+from .views import (
+    UserView, CreateSessionView, DeleteSessionView,
+    DisplayImageView
+    )
 
 app_name = "users"
 
@@ -11,4 +14,6 @@ urlpatterns = [
     path('/session', CreateSessionView.as_view(), name="create_session"),
     
     path('/delete-session', DeleteSessionView.as_view(), name="delete_session"),
+    
+    path('/display-image', DisplayImageView.as_view(), name="display_image"),
 ]
