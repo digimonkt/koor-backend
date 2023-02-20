3f0d2dd6dbcca0e111da27a94f68cbd26b6ba3b7
praveenvaidhya <praveen.vaidhya@digimonk.in>
2023-02-20 17:21:28 +0530

    #33 - create url for get applied jobs

--- job_seekers/urls.py
+++ job_seekers/urls.py
@@ -12,19 +12,19 @@ urlpatterns = [
     path('/about-me', UpdateAboutView.as_view(), name="update_about"),
     
     path('/educations', EducationsView.as_view(), name="educations"),
-    
     path('/educations/<str:educationId>', EducationsView.as_view(), name="educations"),
     
     path('/language', LanguageView.as_view(), name="language"),
-    
     path('/language/<str:languageId>', LanguageView.as_view(), name="language"),
     
     path('/work-experiences', WorkExperiencesView.as_view(), name="work_experiences"),
-    
     path('/work-experiences/<str:workExperienceId>', WorkExperiencesView.as_view(), name="work_experiences"),
     
     path('/skills', SkillsView.as_view(), name="skills"),
     
+    path('/jobs/apply', JobsApplyView.as_view(), name="jobs_apply"),
     path('/jobs/apply/<str:jobId>', JobsApplyView.as_view(), name="jobs_apply"),
+    
+
     
 ]
