20091dbf5d22af3b30a645f2b135ac36604661dd
praveenvaidhya <praveen.vaidhya@digimonk.in>
2023-02-20 16:16:27 +0530

    #33 - show table in admin panel

--- job_seekers/admin.py
+++ job_seekers/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from .models import (
     EducationRecord, JobSeekerLanguageProficiency, EmploymentRecord,
-    JobSeekerSkill
+    JobSeekerSkill, AppliedJob, AppliedJobAttachmentsItem
 )
 
 admin.site.register(EducationRecord)
 admin.site.register(JobSeekerLanguageProficiency)
 admin.site.register(EmploymentRecord)
 admin.site.register(JobSeekerSkill)
+admin.site.register(AppliedJob)
+admin.site.register(AppliedJobAttachmentsItem)
