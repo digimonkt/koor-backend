d0d395af2a1dc8081c23d3bddd2e3546663c5fec
praveenvaidhya <praveen.vaidhya@digimonk.in>
2023-02-21 10:27:17 +0530

    #14 - create delete function for language

--- superadmin/views.py
+++ superadmin/views.py
@@ -463,7 +463,10 @@ class LanguageView(generics.ListAPIView):
         try:
             if self.request.user.is_staff:
                 serializer.is_valid(raise_exception=True)
-                serializer.save()
+                if Language.all_objects.filter(title=serializer.validated_data['title'], is_removed=True).exists():
+                    Language.all_objects.filter(title=serializer.validated_data['title'], is_removed=True).update(is_removed=False)
+                else:
+                    serializer.save()
                 context["data"] = serializer.data
                 return response.Response(
                     data=context,
@@ -486,6 +489,44 @@ class LanguageView(generics.ListAPIView):
                 data=context,
                 status=status.HTTP_400_BAD_REQUEST
             )
+        
+    def delete(self, request, languageId):
+        """
+        Deletes an Language object with the given ID if the authenticated user is a job seeker and owns the
+        Language.
+        Args:
+            request: A DRF request object.
+            educationId: An integer representing the ID of the Language to be deleted.
+        Returns:
+            A DRF response object with a success or error message and appropriate status code.
+        """
+        context = dict()
+        if self.request.user.is_staff:
+            try:
+                Language.objects.get(id=languageId).delete()
+                context['message'] = "Deleted Successfully"
+                return response.Response(
+                    data=context,
+                    status=status.HTTP_200_OK
+                )
+            except Language.DoesNotExist:
+                return response.Response(
+                    data={"Language": "Does Not Exist"},
+                    status=status.HTTP_404_NOT_FOUND
+                )
+            except Exception as e:
+                context["message"] = e
+                return response.Response(
+                    data=context,
+                    status=status.HTTP_404_NOT_FOUND
+                )
+        else:
+            context['message'] = "You do not have permission to perform this action."
+            return response.Response(
+                data=context,
+                status=status.HTTP_401_UNAUTHORIZED
+            )
+
 
 
 class SkillView(generics.ListAPIView):
