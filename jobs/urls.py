2c84728e589751a515dd10472ca9b001b9614c93
praveenvaidhya <praveen.vaidhya@digimonk.in>
2023-02-21 18:37:14 +0530

    #138 - create url for application detail

--- jobs/urls.py
+++ jobs/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from .views import (
     JobSearchView, JobDetailView, JobApplicationsView,
-    RecentApplicationsView
+    RecentApplicationsView, ApplicationsDetailView
 )
 
 app_name = "jobs"
@@ -11,6 +11,7 @@ urlpatterns = [
 
     path('', JobSearchView.as_view(), name="job_search"),
     path('/applications', RecentApplicationsView.as_view(), name="recent_applications"),
+    path('/applications-detail/<str:applicationId>', ApplicationsDetailView.as_view(), name="applications_detail"),
     
     path('/<str:jobId>', JobDetailView.as_view(), name="job_detail"),    
     path('/applications/<str:jobId>', JobApplicationsView.as_view(), name="job_applications"),
