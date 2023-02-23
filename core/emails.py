e2c484dea7d715a0952153277c45600940760aa3
praveenvaidhya <praveen.vaidhya@digimonk.in>
2023-02-23 15:15:20 +0530

    #3 - modify smtp model import

--- core/emails.py
+++ core/emails.py
@@ -4,7 +4,7 @@ from django.core import mail
 from django.core.mail.backends.smtp import EmailBackend
 from django.template.loader import get_template
 
-from conf.models import SMTPSetting
+from superadmin.models import SMTPSetting
 
 
 def get_email_object(subject, email_template_name, context, to_email, content_subtype="html", **kwargs):
@@ -21,9 +21,9 @@ def get_email_object(subject, email_template_name, context, to_email, content_su
         mail_obj = EmailBackend(host=host, port=host_port, password=host_password, username=host_user, use_tls=True,
                                 timeout=10)  # Create mail objects with the SMTP details.
 
-        email_template = get_template(email_template_name).render(context) # Create email templates with valid context.
+        email_template = get_template(email_template_name).render(context)  # Create email templates with valid context.
 
-        if 'type' in kwargs.keys(): # Check any attachment are required or not.
+        if 'type' in kwargs.keys():  # Check any attachment are required or not.
             email_msg = mail.EmailMessage(
                 subject=subject,
                 body=email_template,
