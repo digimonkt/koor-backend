f6cdbb673d918a42c964f15c6524b91d2e5e9ab4
praveenvaidhya <praveen.vaidhya@digimonk.in>
2023-03-03 17:26:31 +0530

    #138 - create job suggestion url

--- jobs/urls.py
+++ jobs/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from .views import (
     JobSearchView, JobDetailView, JobApplicationsView,
-    RecentApplicationsView, ApplicationsDetailView
+    RecentApplicationsView, ApplicationsDetailView, JobSuggestionView
 )
 
 app_name = "jobs"
@@ -10,11 +10,12 @@ app_name = "jobs"
 urlpatterns = [
 
     path('', JobSearchView.as_view(), name="job_search"),
+    
     path('/applications', RecentApplicationsView.as_view(), name="recent_applications"),
+    
     path('/applications-detail/<str:applicationId>', ApplicationsDetailView.as_view(), name="applications_detail"),
     
     path('/<str:jobId>', JobDetailView.as_view(), name="job_detail"),    
     path('/<str:jobId>/applications', JobApplicationsView.as_view(), name="job_applications"),
-
-
+    path('/<str:jobId>/suggestion', JobSuggestionView.as_view(), name="job_suggestion"),
 ]
