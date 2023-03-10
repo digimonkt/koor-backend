70726b15d8754bfd4408ea8f7cf1d14c9e63d8af
praveenvaidhya <praveen.vaidhya@digimonk.in>
2023-03-10 17:36:53 +0530

    #196 - show model in admin

--- superadmin/admin.py
+++ superadmin/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from project_meta.models import EducationLevel, Skill
-from .models import SMTPSetting, Content
+from .models import SMTPSetting, Content, GooglePlaceApi
 
 admin.site.register(EducationLevel)
 admin.site.register(Skill)
 admin.site.register(SMTPSetting)
 admin.site.register(Content)
+admin.site.register(GooglePlaceApi)
