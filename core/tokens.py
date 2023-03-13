89917dbba80b8c5b5450c1d3ed3dde637cf4d2e4
praveenvaidhya <praveen.vaidhya@digimonk.in>
2023-03-13 17:19:36 +0530

    #6 - create jwt token for password reset

--- core/tokens.py
+++ core/tokens.py
@@ -39,3 +39,39 @@ class SessionTokenObtainPairView(TokenObtainPairView):
     The class sets the serializer class as `SessionTokenObtainPairSerializer` which adds the session_id to the token data.
     """
     serializer_class = SessionTokenObtainPairSerializer
+
+class PasswordResetTokenObtainPairSerializer(TokenObtainPairSerializer):
+    """
+    Serializer class that extends the functionality of TokenObtainPairSerializer.
+    The class adds the user_id to the token data.
+    
+    Args:
+    user (User): The User model instance.
+    user_id (str): The user id that needs to be added to the token data.
+    
+    Returns:
+    dict: The token data with added user_id.
+    """
+    @classmethod
+    def get_token(cls, user, user_id):
+        """
+        Method to generate token data by calling the parent class's `get_token` method and adding the user_id to it.
+        
+        Args:
+        user (User): The User model instance.
+        user_id (str): The user id that needs to be added to the token data.
+        
+        Returns:
+        dict: The token data with added user_id.
+        """
+        token = super().get_token(user)
+        del token['session_id']
+        token['user_id'] = str(user_id)
+        return token
+
+class PasswordResetTokenObtainPairView(TokenObtainPairView):
+    """
+    View class that extends the functionality of TokenObtainPairView.
+    The class sets the serializer class as `PasswordResetTokenObtainPairSerializer` which adds the user_id to the token data.
+    """
+    serializer_class = PasswordResetTokenObtainPairSerializer
