163947f2981d9aed668b1cf4a9e388f966979185
praveenvaidhya <praveen.vaidhya@digimonk.in>
2023-03-13 16:11:09 +0530

    #200 - create url path for social login api

--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import (
     UserView, CreateSessionView, DeleteSessionView,
     DisplayImageView, ForgetPasswordView, ChangePasswordView,
-    GetLocationView
+    GetLocationView, SocialLoginView
     )
 
 app_name = "users"
@@ -11,6 +11,8 @@ app_name = "users"
 urlpatterns = [
 
     path('', UserView.as_view(), name="get_user"),
+    
+    path('/social-login', SocialLoginView.as_view(), name="social_login"),
 
     path('/session', CreateSessionView.as_view(), name="create_session"),
     
