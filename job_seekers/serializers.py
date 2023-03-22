546a45ab2b63c26f6d11c1fa71b2a8e9c74382e2
praveenvaidhya <praveen.vaidhya@digimonk.in>
2023-03-22 16:25:14 +0530

    #217 - modify is_saved value in saved job

--- job_seekers/serializers.py
+++ job_seekers/serializers.py
@@ -505,13 +505,14 @@ class GetSavedJobsSerializers(serializers.ModelSerializer):
 
         If the job posting does not exist, an empty dictionary will be returned.
         """
-
-        context = dict()
+        return_context = dict()
         try:
-            get_data = GetJobsDetailSerializers(obj.job)
-            if get_data.data:
-                context = get_data.data
+            if 'request' in self.context:
+                user = self.context['request'].user
+                get_data = GetJobsDetailSerializers(obj.job, context={"user": user})
+                if get_data.data:
+                    return_context = get_data.data
         except JobDetails.DoesNotExist:
             pass
         finally:
-            return context
+            return return_context
