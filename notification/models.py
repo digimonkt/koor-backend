71595ebccf65eefeb7d30386a4e8cdddf94c5f01
praveenvaidhya <praveen.vaidhya@digimonk.in>
2023-03-23 13:02:42 +0530

    #229 - write doc string

--- notification/models.py
+++ notification/models.py
@@ -13,7 +13,28 @@ from job_seekers.models import AppliedJob
 
 from jobs.models import JobFilters
 
+
 class Notification(BaseModel, SoftDeleteModel, TimeStampedModel, models.Model):
+    """
+    A model that represents a notification sent to a user.
+
+    Attributes:
+        - `user (ForeignKey)`: The user that the notification is intended for.
+        - `notification_type (CharField)`: The type of the notification.
+        - `application (ForeignKey)`: The applied job associated with the notification (if applicable).
+        - `job_filter (ForeignKey)`: The job filter associated with the notification (if applicable).
+        - `seen (BooleanField)`: Whether the notification has been seen by the user.
+
+    Methods:
+        __str__(): Returns a string representation of the notification.
+
+    Meta:
+        - `verbose_name (str)`: The singular name for the model.
+        - `verbose_name_plural (str)`: The plural name for the model.
+        - `db_table (str)`: The name of the database table to use for the model.
+        - `ordering (list)`: The default ordering for the model.
+    """
+
     NOTIFICATION_TYPE_CHOICE = (
         ('applied', "Applied"),
         ('password update', "Password Updated"),
@@ -59,13 +80,12 @@ class Notification(BaseModel, SoftDeleteModel, TimeStampedModel, models.Model):
         db_column="seen",
         default=False
     )
-    
+
     def __str__(self):
         return str(self.notification_type) + '(' + str(self.user.id) + ')'
-    
+
     class Meta:
         verbose_name = "Notification"
         verbose_name_plural = "Notifications"
         db_table = "Notification"
         ordering = ['-created']
-        
