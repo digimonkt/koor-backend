5ff710b7098a69a0cb7f853311c9593ca91b7bfc
praveenvaidhya <praveen.vaidhya@digimonk.in>
2023-03-31 18:44:35 +0530

    #257 - show table in admin

--- job_seekers/admin.py
+++ job_seekers/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import (
     EducationRecord, JobSeekerLanguageProficiency, EmploymentRecord,
     JobSeekerSkill, AppliedJob, AppliedJobAttachmentsItem,
-    SavedJob, JobPreferences
+    SavedJob, JobPreferences, Categories
 )
 
 admin.site.register(EducationRecord)
@@ -13,3 +13,4 @@ admin.site.register(AppliedJob)
 admin.site.register(AppliedJobAttachmentsItem)
 admin.site.register(SavedJob)
 admin.site.register(JobPreferences)
+admin.site.register(Categories)
