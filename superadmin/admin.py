5d4dd9c740fe53ab912dd1500c220df25993d651
praveenvaidhya <praveen.vaidhya@digimonk.in>
2023-04-20 16:46:35 +0530

    #347 - show AllCountry table in admin panel

--- superadmin/admin.py
+++ superadmin/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from project_meta.models import EducationLevel, Skill, JobSeekerCategory
+from project_meta.models import (
+    EducationLevel, Skill, JobSeekerCategory,
+    AllCountry
+)
 from .models import SMTPSetting, Content, GooglePlaceApi
 
 admin.site.register(EducationLevel)
@@ -8,3 +11,4 @@ admin.site.register(SMTPSetting)
 admin.site.register(Content)
 admin.site.register(GooglePlaceApi)
 admin.site.register(JobSeekerCategory)
+admin.site.register(AllCountry)
