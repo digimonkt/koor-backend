aaf593b914f101d973cf329e88baee824a65e560
praveenvaidhya <praveen.vaidhya@digimonk.in>
2023-05-12 18:07:17 +0530

    #385 - show table in admin

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Conversation, ConversationUser, ChatMessage
 
-# Register your models here.
+admin.site.register(Conversation)
+admin.site.register(ConversationUser)
+admin.site.register(ChatMessage)
+
