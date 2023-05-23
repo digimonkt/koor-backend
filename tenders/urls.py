a8c18b837b85db9afb8aa7486f02d1cb8ace9e09
praveenvaidhya <praveen.vaidhya@digimonk.in>
2023-05-23 15:17:01 +0530

    #459 - create url for get applicaton detail

--- tenders/urls.py
+++ tenders/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from .views import (
     TenderSearchView, TenderDetailView, TenderFilterView,
-    TenderSuggestionView
+    TenderSuggestionView, ApplicationsDetailView
 )
 
 app_name = "tenders"
@@ -13,6 +13,8 @@ urlpatterns = [
     
     path('/filter', TenderFilterView.as_view(), name="tender_filter"),
     path('/filter/<str:filterId>', TenderFilterView.as_view(), name="tender_filter"),
+    
+    path('/applications-detail/<str:applicationId>', ApplicationsDetailView.as_view(), name="applications_detail"),
     
     path('/<str:tenderId>', TenderDetailView.as_view(), name="tender_detail"),
     path('/<str:tenderId>/suggestion', TenderSuggestionView.as_view(), name="tender_suggestion"),  
