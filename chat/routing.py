4bbce5bd4cf929f61b9f911e5dd877ab1d3d089d
praveenvaidhya <praveen.vaidhya@digimonk.in>
2023-06-07 15:35:25 +0530

    #385 - modify chat url

--- chat/routing.py
+++ chat/routing.py
@@ -4,6 +4,6 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r"ws/chat/$", consumers.ChatConsumer.as_asgi()),
-    re_path(r"ws/chat_activity/$", consumers.ChatActivityConsumer.as_asgi(), name="chat_activity"),
+    re_path(r"ws/chat$", consumers.ChatConsumer.as_asgi()),
+    re_path(r"ws/chat_activity$", consumers.ChatActivityConsumer.as_asgi(), name="chat_activity"),
 ]
