c0b71b09d425ba6de3f93c35621db8343615631c
praveenvaidhya <praveen.vaidhya@digimonk.in>
2023-06-07 12:06:28 +0530

    #7 - send session id in response

--- users/views.py
+++ users/views.py
@@ -194,6 +194,7 @@ class UserView(generics.GenericAPIView):
                         status=status.HTTP_401_UNAUTHORIZED
                     )
             user_data = User.objects.get(id=user_id)
+            session_id = user_data.users_usersession_user.last().id
             if user_data.role == "job_seeker":
                 get_data = JobSeekerDetailSerializers(user_data)
                 context = get_data.data
@@ -203,7 +204,7 @@ class UserView(generics.GenericAPIView):
             elif user_data.role == "vendor":
                 get_data = VendorDetailSerializers(user_data)
                 context = get_data.data
-
+            context['session_id'] = session_id
             return response.Response(
                 data=context,
                 status=status.HTTP_200_OK
