790d4547a917df9f43bf1b1e138d5c7fdc24a08a
praveenvaidhya <praveen.vaidhya@digimonk.in>
2023-06-13 16:34:32 +0530

    #160 - call slugify

--- superadmin/models.py
+++ superadmin/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils.translation import gettext as _
+from django.template.defaultfilters import slugify
 from django.contrib.postgres.fields import ArrayField
 
 from core.models import (
