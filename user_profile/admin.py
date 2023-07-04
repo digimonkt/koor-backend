0f50d34eba61cc8774470379ccd6e8983a49c593
praveenvaidhya <praveen.vaidhya@digimonk.in>
2023-07-04 13:33:26 +0530

    #245 - show UserAnalytic model in admin

--- user_profile/admin.py
+++ user_profile/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import (
     JobSeekerProfile, EmployerProfile, UserFilters,
-    VendorProfile
+    VendorProfile, UserAnalytic
 )
 
 # Register your models here.
@@ -9,3 +9,4 @@ admin.site.register(JobSeekerProfile)
 admin.site.register(EmployerProfile)
 admin.site.register(VendorProfile)
 admin.site.register(UserFilters)
+admin.site.register(UserAnalytic)
