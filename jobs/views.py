1b90e590b28a8b1aa2c0241b98c53f54249bedef
praveenvaidhya <praveen.vaidhya@digimonk.in>
2023-10-26 16:28:39 +0530

    #619 - add error message if invalid interview date

--- jobs/views.py
+++ jobs/views.py
@@ -476,10 +476,19 @@ class ApplicationsDetailView(generics.GenericAPIView):
                                 status=status.HTTP_400_BAD_REQUEST
                             )
                     elif action == "planned_interviews":
+                        print('test')
                         if application_status.rejected_at is None:
+                            print(datetime.now().date())
+                            print(request.data['interview_at'])
                             if 'interview_at' in request.data:
-                                if application_status.interview_at:
-                                    message = "Already "
+                                interview_at_str = request.data['interview_at']
+                                interview_at_datetime = datetime.strptime(interview_at_str, '%Y-%m-%dT%H:%M:%S')
+                                current_datetime = datetime.now()
+                                if interview_at_datetime < current_datetime:
+                                    return response.Response(
+                                        data={"message": ["Interview date in invalid."]},
+                                        status=status.HTTP_400_BAD_REQUEST
+                                    )
                                 else:
                                     if application_status.shortlisted_at is None:
                                         application_status.shortlisted_at = datetime.now()
@@ -502,7 +511,8 @@ class ApplicationsDetailView(generics.GenericAPIView):
                                             if application_status.user.get_email:
                                                 get_email_object(
                                                     subject=f'Notification for interview planned',
-                                                    email_template_name='email-templates/send-notification.html',
+                                                    email_template_name='email-templates/new/notification.html',
+                                                    # email_template_name='email-templates/send-notification.html',
                                                     context=email_context,
                                                     to_email=[application_status.user.email, ]
                                                 )
