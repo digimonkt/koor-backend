b3ac740ef88ffbd1ae6310991b4fd98e334d1ac8
praveenvaidhya <praveen.vaidhya@digimonk.in>
2023-11-22 13:05:26 +0530

    #619 - modify otp verification email template

--- users/views.py
+++ users/views.py
@@ -126,7 +126,7 @@ class UserView(generics.GenericAPIView):
                 context["yourname"] = user.email
                 context["otp"] = otp
                 get_email_object(
-                    subject=f'OTP for Verification',
+                    subject=f'New Registration for Koor Jobs',
                     email_template_name='email-templates/new/new-login-detected.html',
                     # email_template_name='email-templates/send-forget-password-otp.html',
                     context=context,
@@ -393,7 +393,7 @@ class SendOtpView(generics.GenericAPIView):
                 context["otp"] = otp
                 get_email_object(
                     subject=f'OTP for Verification',
-                    email_template_name='email-templates/new/new-login-detected.html',
+                    email_template_name='email-templates/new/otp-verification.html',
                     # email_template_name='email-templates/send-forget-password-otp.html',
                     context=context,
                     to_email=[user_email, ]
