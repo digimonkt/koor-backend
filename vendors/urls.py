aa15bf204eb449f66b7d674f30ff45dbe6889050
praveenvaidhya <praveen.vaidhya@digimonk.in>
2023-11-24 15:04:23 +0530

    #619 - create function for apply for email function

--- vendors/urls.py
+++ vendors/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from .views import (
     UpdateAboutView, TenderSaveView, TenderApplyView,
-    SectorView, TagView
+    SectorView, TagView, TenderApplyByEmailView
 )
 
 app_name = "vendors"
@@ -19,6 +19,7 @@ urlpatterns = [
     path('/tender/save/<str:tenderId>', TenderSaveView.as_view(), name="tender_save"),
     
     path('/tender/apply', TenderApplyView.as_view(), name="tender_apply"),
+    path('/tender/apply/by_email/<str:tenderId>', TenderApplyByEmailView.as_view(), name="tender_apply_by_email"),
     path('/tender/apply/<str:tenderId>', TenderApplyView.as_view(), name="tender_apply"),
 
 ]
