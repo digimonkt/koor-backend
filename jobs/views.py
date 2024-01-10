b24578fd0e46b7137dec24a92e06b90530a4fa11
praveenvaidhya <praveen.vaidhya@digimonk.in>
2024-01-10 11:56:59 +0530

    #743 - modify talent count in job categories api

--- jobs/views.py
+++ jobs/views.py
@@ -986,9 +986,11 @@ class JobCategoryView(generics.ListAPIView):
         # Retrieve the top job categories and their counts of associated talents
         all_talents = JobCategory.objects.annotate(
             category_count=Count(
-                'jobs_jobsubcategory_categories__job_seekers_categories_categories',
+                'jobs_jobsubcategory_categories__job_seekers_categories_categories__user',
                 distinct=True,
-                filter=Q(jobs_jobsubcategory_categories__job_seekers_categories_categories__user__display_in_search=True)
+                filter=Q(
+                    jobs_jobsubcategory_categories__job_seekers_categories_categories__user__job_seekers_jobpreferences_user__display_in_search=True
+                )
             )
         ).order_by('-category_count')[:5]
 
